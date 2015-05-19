6246440d7ce28c5fe1abed394527b90bda4f9007
miloszw <milosz@spotify.com>
2015-05-19 16:31:35 +0200

    Add basic graph canvas. Add new nodes on click.

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,5 +1,25 @@
 jsPlumb.ready(function() {
-  var instance = jsPlumb.getInstance({
+  var jsp = jsPlumb.getInstance({
     Container: "container"
   });
-})
+
+  // append new node to graph
+  var addNode = function(e) {
+    var node = $("<div class=\"node\"><p class=\"label\">New Node</p></div>");
+    // set correct position within the graph
+    node.css("left", e.pageX - this.offsetLeft);
+    node.css("top", e.pageY - this.offsetTop);
+    jsp.draggable(node);  // make draggable
+    node.click(false);    // disable addNode handler on node
+    $(this).append(node); // append
+  };
+
+  // add new nodes on click
+  $("div#container")
+    .on("click", addNode)
+    // stop event propagation to disable handler on children elements
+    .children().on("click", false);
+
+  // dbg: export jsp instance
+  window.jsp = jsp;
+});
