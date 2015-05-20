4ff86f78379cc593ad38d16d57a6ba6048324ee2
miloszw <milosz@spotify.com>
2015-05-20 14:59:35 +0200

    Implement node selection

--- scripts/editor.js
+++ scripts/editor.js
@@ -5,18 +5,41 @@ jsPlumb.ready(function() {
 
   // append new node to graph
   var addNode = function(e) {
-    var node = $("<div class=\"node\"><p class=\"label\">New Node</p></div>");
+    var node = $("<div class=\"node\"/>");
+    node.append("<p class=\"label\">New Node</p>");
+
     // set correct position within the graph
     node.css("left", e.pageX - this.offsetLeft);
     node.css("top", e.pageY - this.offsetTop);
-    jsp.draggable(node);  // make draggable
-    node.click(false);    // disable addNode handler on node
-    $(this).append(node); // append
+
+    // append node to graph
+    $(this).append(node);
+    node.on("click", selectNode);
+    jsp.draggable(node, {containment: true});
+    jsp.setDraggable(node, false);
+
+  };
+  var selectNode = function() {
+    deselectAll();
+    $(this).addClass("selected");
+    jsp.setDraggable($(this), true);
+    jsp.setSourceEnabled($(this), false);
+
+    return false; // don't propagate - otherwise addNode is called
+  };
+  var deselectAll = function() {
+    var selected = $(".node.selected");
+    if (selected.length > 0) {
+      jsp.setDraggable(selected, false);
+      jsp.setSourceEnabled(selected, true);
+      $(".node.selected").removeClass("selected");
+    }
   };
 
   // add new nodes on click
   $("div#container")
-    .on("click", addNode)
+    .on("dblclick", addNode)
+    .on("click", deselectAll)
     // stop event propagation to disable handler on children elements
     .children().on("click", false);
 
