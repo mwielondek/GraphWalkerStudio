29432380ecae506d298572ca7d67b1e41e3a8e98
miloszw <milosz@spotify.com>
2015-08-04 10:04:03 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if ! command -v riot >/dev/null 2>&1; then
+  echo "[!] Riot compiler required. Install with 'npm install riotjs -g'." >&2
+  exit 1
+else
+  echo "[*] Compiling riot tags"
+  cd "scripts/tags"
+  ./compile.sh js && echo "[✓] Done." || echo "[✗] Error occured."
+fi
--- scripts/tags/compile.sh
+++ scripts/tags/compile.sh
@@ -1,9 +1,11 @@
 if [ $# -eq 0 ]
 then
   echo "usage: $0 <outputdir>"
+  exit 1
 else
   [ -d $1 ] || mkdir $1
   cat *.tag > $1/compiled.tag && cd $1
   riot -m compiled.tag Studio.js
   rm compiled.tag
+  exit 0
 fi
