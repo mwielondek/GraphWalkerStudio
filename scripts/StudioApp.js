9794b51e7149ed20c9a93d45c2f6746990e7ca95
miloszw <milosz@spotify.com>
2015-08-06 14:07:43 +0200

    Add tagUtils mixin

--- scripts/StudioApp.js
+++ scripts/StudioApp.js
@@ -2,9 +2,19 @@ define(['riot', 'store/VertexStore', 'store/EdgeStore', 'store/ConnectionStore',
  'store/ModelStore', 'tag/Studio', 'utils/mixins'],
 function(riot) {
 
+  var tagUtils = {
+    // Toggles boolean variable
+    toggle: function(variable) {
+      return function() {
+        this[variable] = !this[variable];
+      };
+    }
+  };
+
   var init = function(opts) {
     // Mount the studio tag and return the mounted instance
     var opts = opts || {}
+    riot.mixin('tagUtils', tagUtils);
     return riot.mount('studio', opts);
   }
 
