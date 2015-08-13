9044cac325035af0a5efe703231ca6923ef016fa
miloszw <milosz@spotify.com>
2015-08-13 12:14:06 +0200

    Remove bindFirst from mixins

--- scripts/utils/mixins.js
+++ scripts/utils/mixins.js
@@ -79,15 +79,4 @@ define(['jquery'], function($) {
     return true;
   };
 
-  // Bind an event handler to execute first
-  $.fn.bindFirst = function(name, fn) {
-    var elem, handlers, i, _len;
-    this.on(name, fn);
-    for (i = 0, _len = this.length; i < _len; i++) {
-      elem = this[i];
-      handlers = jQuery._data(elem).events[name.split('.')[0]];
-      handlers.unshift(handlers.pop());
-    }
-  };
-
 });
