422df75fb43e2b11ba19717c40b539c2a32a23d5
miloszw <milosz@spotify.com>
2015-08-13 12:12:48 +0200

    Change Enum constructor to not use `this` and freeze the returned obj

--- scripts/constants/Utils.js
+++ scripts/constants/Utils.js
@@ -1,13 +1,15 @@
 define(function() {
   'use strict';
-  
+
   return {
     // Creates an enum object => {ENUM: 'ENUM'}
     Enum: function(constantsList) {
+      var constants = {};
       for (var i = 0; i < constantsList.length; i++) {
         var constant = constantsList[i];
-        this[constant] = constant;
+        constants[constant] = constant;
       }
+      return Object.freeze(constants);
     }
 
   };
